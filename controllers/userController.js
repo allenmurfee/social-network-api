747017d190db56fcffeff383e42fea35e1a6e514
allenmurfee <allenmurfee123@gmail.com>
2022-12-16 15:38:39 -0500

    worked more on user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,26 @@ module.exports = {
       return res.status(404).json(err);
     }
   },
+  async deleteUser(req, res) {
+    try {
+      const deleteUser = await User.findOneAndRemove({
+        _id: req.params.userId,
+      });
+      return res.json(deleteUser).status(200);
+    } catch (err) {
+      return res.status(404).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const updateUser = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      return res.json(updateUser).status(200);
+    } catch (err) {
+      return res.status(404).json(err);
+    }
+  },
 };
