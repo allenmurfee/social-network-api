6d51de1ddfcde797b79b395c3db973eff5a4b99e
allenmurfee <allenmurfee123@gmail.com>
2022-12-15 21:22:36 -0500

    added Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,22 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: { type: Date },
+    username: { type: String, required: true },
+    //array of nested documents, has to be array
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
